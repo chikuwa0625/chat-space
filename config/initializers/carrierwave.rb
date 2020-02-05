da93d1dfb29394fa3a2734e796df6e9b0c20c1f1
chikuwa0625 <57427219+chikuwa0625@users.noreply.github.com>
2020-02-05 19:14:05 +0900

    change_carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-spaceimagee'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/:chat-spaceimagee'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-spaceimagee'
 end
